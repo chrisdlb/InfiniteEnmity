ec9cce43142418504fd9c67483ded761d19d32f5
Christopher Bennett <chris@benza.co.nz>
2016-11-19 20:16:08 +1300

    Updated website URLs
    Updated website URLs to new site.
    http://ie.chrisdlb.co.nz/

--- Assets/scripts/UI/URLS.cs
+++ Assets/scripts/UI/URLS.cs
@@ -6,14 +6,14 @@ public class URLS : MonoBehaviour {
 	
 	public void Feedback () {
 
-        Application.OpenURL("http://content.chrisdlb.co.nz/projects/games/infiniteenmity/index.php");
+        Application.OpenURL("http://ie.chrisdlb.co.nz/about.php");
 
     }
 
     public void Credits()
     {
 
-        Application.OpenURL("http://content.chrisdlb.co.nz/projects/games/infiniteenmity/credits.php");
+        Application.OpenURL("http://ie.chrisdlb.co.nz/about.php");
 
     }
 }
